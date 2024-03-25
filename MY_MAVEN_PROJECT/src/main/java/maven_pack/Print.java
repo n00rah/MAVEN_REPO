fafa96d4e5f6586d5358b6bd28f1a8ca77feb451
NOORA <NOORA@192.168.1.33>
2024-03-25 11:50:58 +0530

    MY FIRST COMMIT

---
+++ MY_MAVEN_PROJECT/src/main/java/maven_pack/Print.java
@@ -0,0 +1,10 @@
+package maven_pack;
+
+public class Print {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("MY FIRST MAVEN PROJECT");
+	}
+
+}
---
+++ MY_MAVEN_PROJECT/src/main/java/maven_pack/Sum.java
@@ -0,0 +1,11 @@
+package maven_pack;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10,b=5,c=a+b;
+System.out.println("THE SUM IS "+c);
+	}
+
+}
